9ca3a611e1758879a6ee09d05110b52dee70d742
PSR <paul@mydatause.com>
2018-09-14 18:06:19 -0700

    Added weeks 12 to 14 homework

---
+++ Web Scraping/config.py
@@ -0,0 +1,5 @@
+#Twitter API Keys
+consumer_key = "dZ7TYBvoZyC8XnmdHJUniVCHu"
+consumer_secret = "0ERlMtbzLJIrrWjgMyGPWaJpNVmRMqRjlbYaVe1HCBWUjtvSkP"
+access_token = "1019763119516626944-6xhvNCDdXR1VkwWBiWvUD8VDMpJeh0"
+access_token_secret = "GGVc50MBuuvo2TasE0LbNejqsVfPTwmsoR5HLcEqFDbCx"
